b0a2badfe6cc3bf659d7bb4e803626810fd8845d
Shashindu <shashinduchamika99@gmail.com>
2023-07-04 18:38:31 +0530

    sn

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import WelcomeSlide from './component/welcome_slide/WelcomeSlide';
 function App() {
   return (
     <div className="App">
-      {/* <NavBar></NavBar>
-      <WelcomeSlide></WelcomeSlide> */}
+      <NavBar></NavBar>
+      <WelcomeSlide></WelcomeSlide>
       <h1>Hello world</h1>
     </div>
   );
